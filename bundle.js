b6e8f7184dce85b0375cefd3c1edc155d4a50adb
Howard <howardl93@yahoo.com>
2019-11-12 13:22:16 -0800

    fixed https protocol

--- bundle.js
+++ bundle.js
@@ -24130,7 +24130,7 @@ __webpack_require__.r(__webpack_exports__);
 function fetchSearchGiphys(searchTerm) {
   return $.ajax({
     method: 'GET',
-    url: "http://api.giphy.com/v1/gifs/search?q=".concat(searchTerm, "&api_key=dc6zaTOxFJmzC&limit=2")
+    url: "https://api.giphy.com/v1/gifs/search?q=".concat(searchTerm, "&api_key=dc6zaTOxFJmzC&limit=2")
   });
 }
 
